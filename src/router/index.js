d25e8268b2db8df2aa4a923d6a70f507861ce4a7
Jin <141120215+Lyly43@users.noreply.github.com>
2026-03-29 11:23:13 +0700

    fix router hash

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/DDH/'),
+  history: createWebHistory(),
   routes: routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 }
