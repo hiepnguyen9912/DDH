c1e1424b57bc5c745dea16640bb59a298de4c4d8
Jin <141120215+Lyly43@users.noreply.github.com>
2026-03-29 11:12:00 +0700

    deloy new

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Default from './layout/wrapper/index.vue'
-import Blank from './layout/wrapper/index_blank.vue'
+// import Default from './layout/wrapper/index.vue'
+// import Blank from './layout/wrapper/index_blank.vue'
 import Client from './layout/wrapper/index_client.vue'
 import Toaster from "@meforma/vue-toaster";
 const app = createApp(App)
@@ -11,8 +11,8 @@ app.use(router)
 app.use(Toaster, {
     position: "top-right",
   });
-app.component("default-layout", Default);
-app.component("blank-layout", Blank);
+// app.component("default-layout", Default);
+// app.component("blank-layout", Blank);
 app.component("client-layout", Client);
 Client
 app.mount("#app")
